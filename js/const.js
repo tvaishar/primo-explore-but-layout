4debc3796791920b9fb286241fde26b855d2d248
tvaishar <36225733+tvaishar@users.noreply.github.com>
2018-09-05 12:52:52 +0200

    view from url
    now it searches vid=viewName anywhere in url

--- js/const.js
+++ js/const.js
@@ -1,6 +1,9 @@
 /* consts begin*/
 var searchArray = window.location.search.split('&');
-var viewArray = searchArray[0].split('=');
+function findView(url_fragment) {
+    return url_fragment.search('vid')+1; //bool value created (search returns -1 when not found, we need 0 for false)
+}
+var viewArray = searchArray[searchArray.findIndex(findView)].split('='); 
 var viewName = viewArray[1];
 
 /*viewName="420BUT_AUGUPD";*/
