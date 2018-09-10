aac1dccb0c2254ad369adf6677ae8a62f451060b
tvaishar <36225733+tvaishar@users.noreply.github.com>
2018-09-10 15:17:52 +0200

    support for tips
    some vars added

--- js/const.js
+++ js/const.js
@@ -1,11 +1,29 @@
 /* consts begin*/
 var searchArray = window.location.search.split('&');
 function findView(url_fragment) {
-    return url_fragment.search('vid')+1; //bool value created (search returns -1 when not found, we need 0 for false)
+  return url_fragment.search('vid') + 1; //bool value created (search returns -1 when not found, we need 0 for false)
 }
-var viewArray = searchArray[searchArray.findIndex(findView)].split('='); 
+function findLang(url_fragment) {
+  return url_fragment.search('lang') + 1; //bool value created (search returns -1 when not found, we need 0 for false)
+}
+var viewArray = searchArray[searchArray.findIndex(findView)].split('=');
 var viewName = viewArray[1];
 
 /*viewName="420BUT_AUGUPD";*/
+
+if (searchArray.findIndex(findLang) == -1 )
+		{
+		var viewLang = "cs_CZ";	
+		}
+else
+		{
+		var viewArray = searchArray[searchArray.findIndex(findLang)].split('=');
+		var viewLang = viewArray[1];	
+		}
+
+var tipNumber = Math.floor(Math.random() * 17); //16 tips
+
+var viewLang = "cs_CZ";	
+/* tipNumber=1; */
 
 /* consts end*/
