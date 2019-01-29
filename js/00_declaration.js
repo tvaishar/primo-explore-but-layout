d48f1a13fa187cf12ff3d47405db70407778ebbf
tvaishar <36225733+tvaishar@users.noreply.github.com>
2019-01-29 11:23:05 +0100

    declaration
    00_declaration.js added, 00_consts.js renamed to 01_consts.js

---
+++ js/00_declaration.js
@@ -0,0 +1,6 @@
+/* app variable declaration must be in separate file, because when merging, gulp tends to erase it from custom.js */
+
+    var app = angular.module('viewCustom', ['angularLoad']);
+
+
+   
